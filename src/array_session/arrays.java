3386ac74803a9896def0357f1593cb4e2e2e1097
manukotari <manojkumarkumar01189@gmail.com>
2025-06-26 19:09:54 +0530

    array creation and accessing

--- src/array_session/arrays.java
+++ src/array_session/arrays.java
@@ -1,5 +1,16 @@
 package array_session;
 
 public class arrays {
+	public static void main(String[] args) {
+		int  a[]= {1,2,4,5,6};
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
 
+		int[] n=new int[3];
+		for(int i=0;i<n.length;i++) {
+			System.out.println(1);
+		}
+	}
 }
